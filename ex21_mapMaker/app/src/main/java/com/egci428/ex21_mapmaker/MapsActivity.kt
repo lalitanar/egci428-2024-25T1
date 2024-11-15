56a181cc285638212477ce6a311ee10289c4b28f
Lalita N. <32094592+lalitanar@users.noreply.github.com>
2024-11-15 07:14:05 +0000

    ex21_mapMaker-v3

--- ex21_mapMaker/app/src/main/java/com/egci428/ex21_mapmaker/MapsActivity.kt
+++ ex21_mapMaker/app/src/main/java/com/egci428/ex21_mapmaker/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.egci428.ex21_mapmaker
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,11 +11,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.egci428.ex21_mapmaker.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    var countMarker: Int = 0
+    private var preLatLon = LatLng(0.0,0.0)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +49,58 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        //Add a marker in Sydney and move the camera
+        /*val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,14F))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,9F))*/
+
+        mMap.setOnMapClickListener {
+
+            latlon ->
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlon,5F))
+            mMap.addMarker(MarkerOptions()
+                .position(latlon)
+                .title("lat:"+latlon.latitude.toString()+" lon: "+latlon.longitude.toString())
+                .snippet(countMarker.toString()))
+
+                if (countMarker>0) {
+                    mMap.addPolyline(PolylineOptions()
+                        .add(preLatLon, latlon)
+                        .width(5F)
+                        .color(Color.RED)
+                    )
+                }
+                preLatLon = latlon
+                countMarker++
+        }
+
+
+
+        /*mMap.setOnMapLongClickListener {
+            latlon -> mMap.addMarker(MarkerOptions()
+                .position(latlon)
+            //.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.image10101))
+                .title("lat:"+latlon.latitude.toString()+" lon: "+latlon.longitude.toString())
+                .snippet("egci428")
+            )
+        }*/
+
+        /*mMap.addPolyline(PolylineOptions()
+            .add(LatLng(15.0, 100.45), LatLng(15.432, 100.456), LatLng(15.5, 100.7))
+            .width(5F)
+            .color(Color.RED)
+        )
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(15.0, 100.45),9F))*/
+
+        /*mMap.addPolygon(
+            PolygonOptions()
+            .add(LatLng(15.0, 100.45), LatLng(15.432, 100.456), LatLng(15.5, 100.7), LatLng(15.0, 100.45))
+            .strokeColor(Color.BLUE)
+            .fillColor(Color.BLUE)
+        )
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(15.0, 100.45),9F))*/
+
     }
 }
